44b761676e4bc66eb5c55e27dd67620d8af8c76a
alnohl <150450275+alnohl@users.noreply.github.com>
2023-12-01 17:41:44 +0100

    Update AN_activity_wilcoxon_test_grouped.R

--- Statistical_Analysis/Activity/AN_activity_wilcoxon_test_grouped.R
+++ Statistical_Analysis/Activity/AN_activity_wilcoxon_test_grouped.R
@@ -4,170 +4,69 @@ library(ggpubr)
 library(rstatix)
 
 
-# Set the file path for the beta values
-beta_values_WordsVsBaseline <- "/Users/alexandranohl/Documents/MSc IDB/Master thesis/Projects/Localizer/MRI Data Analysis/Analysis_children/Statistical_analysis/Activity/beta Activity visfAtlas/beta_Values_visfAtlas_WordsVsBaseline.txt"
-beta_values_WordsVsFaces <- "/Users/alexandranohl/Documents/MSc IDB/Master thesis/Projects/Localizer/MRI Data Analysis/Analysis_children/Statistical_analysis/Activity/beta Activity visfAtlas/beta_Values_visfAtlas_WordsVsFaces.txt"
-beta_values_FacesVsBaseline <- "/Users/alexandranohl/Documents/MSc IDB/Master thesis/Projects/Localizer/MRI Data Analysis/Analysis_children/Statistical_analysis/Activity/beta Activity visfAtlas/beta_Values_visfAtlas_FacesVsBaseline.txt"
-beta_values_FacesVsWords <- "/Users/alexandranohl/Documents/MSc IDB/Master thesis/Projects/Localizer/MRI Data Analysis/Analysis_children/Statistical_analysis/Activity/beta Activity visfAtlas/beta_Values_visfAtlas_FacesVsWords.txt"
-
+# Set the file path for your values of interest for t-test (or Wilxocon test)
+file_path <- "/path/to/your/data/for/t-test/data.txt"
 
 
 # Read data from TXT files for beta values
-beta_WB <- read.table(beta_values_WordsVsBaseline, header = TRUE)
-beta_WF <- read.table(beta_values_WordsVsFaces, header = TRUE)
-beta_FB <- read.table(beta_values_FacesVsBaseline, header = TRUE)
-beta_FW <- read.table(beta_values_FacesVsWords, header = TRUE)
+table <- read.table(file_path, header = TRUE)
 
 
 # compute differences between the two groups
-beta_WB <- beta_WB %>% mutate(differences = WordsVsBaseline_LH - WordsVsBaseline_RH)
-beta_WF <- beta_WF %>% mutate(differences = WordsVsFaces_LH - WordsVsFaces_RH)
-beta_FB <- beta_FB %>% mutate(differences = FacesVsBaseline_LH - FacesVsBaseline_RH)
-beta_FW <- beta_FW %>% mutate(differences = FacesVsWords_LH - FacesVsWords_RH)
+table <- table %>% mutate(differences = group1 - group2)
 
 
+## ASSUMPTIONS AND PRELIMINARY TESTS
 
-
-## Assumptions and preliminary tests
-# test for outliers
+## test for outliers
 # –> the resulting column 'is.extreme' should be FALSE for all participants shown
-beta_WB %>% identify_outliers(differences)
-beta_WF %>% identify_outliers(differences)
-beta_FB %>% identify_outliers(differences)
-beta_FW %>% identify_outliers(differences)
+table %>% identify_outliers(differences)
 
 
 ## exclude outliers
-
-beta_WB_nooutliers <- beta_WB[!(row.names(beta_WB) %in% c("90")),]
-beta_WF_nooutliers <- beta_WF[!(row.names(beta_WF) %in% c("27","41","48","90")),]
-beta_FB_nooutliers <- beta_FB[!(row.names(beta_FB) %in% c("32","44","48","83","90")),]
-beta_FW_nooutliers <- beta_FW[!(row.names(beta_FW) %in% c("27","41","48")),]
+nooutliers <- table[!(row.names(table) %in% c("1","2","3")),] # enter the row number of all outliers (of both group 1 and group 2; here 1, 2, and 3)
 
 
-## Transform into long data (gather the LH and RH values in the same column)
-beta_WB_nooutliers.long <- beta_WB_nooutliers %>%
-  gather(key = "group", value = "WordsVsBaseline", WordsVsBaseline_LH, WordsVsBaseline_RH)
-head(beta_WB_nooutliers.long, 3)
-
-beta_WF_nooutliers.long <- beta_WF_nooutliers %>%
-  gather(key = "group", value = "WordsVsFaces", WordsVsFaces_LH, WordsVsFaces_RH)
-head(beta_WF_nooutliers.long, 3)
-
-beta_FB_nooutliers.long <- beta_FB_nooutliers %>%
-  gather(key = "group", value = "FacesVsBaseline", FacesVsBaseline_LH, FacesVsBaseline_RH)
-head(beta_FB_nooutliers.long, 3)
-
-beta_FW_nooutliers.long <- beta_FW_nooutliers %>%
-  gather(key = "group", value = "FacesVsWords", FacesVsWords_LH, FacesVsWords_RH)
-head(beta_FW_nooutliers.long, 3)
-
-
-# Shapiro-Wilk normality test for the differences
-beta_WB_nooutliers %>% shapiro_test(differences) # p-value = 0.587
-beta_WF_nooutliers %>% shapiro_test(differences) # p-value = 0.659
-beta_FB_nooutliers %>% shapiro_test(differences) # p-value = 0.00953
-beta_FW_nooutliers %>% shapiro_test(differences) # p-value = 0.136
-
+## Shapiro-Wilk normality test for the differences
+# normal distribution if p > 0.05 (use t-test), not normal distributed if p < 0.05 (use Wilcoxon test)
+nooutliers %>% shapiro_test(differences)
 
 # QQ plot for the difference
-ggqqplot(beta_WB_nooutliers, "differences")
-ggqqplot(beta_WF_nooutliers, "differences")
-ggqqplot(beta_FB_nooutliers, "differences")
-ggqqplot(beta_FW_nooutliers, "differences")
+ggqqplot(nooutliers, "differences")
 
 
+
+## Transform into long data (gather the values of group 1 and group 2 in the same column)
+nooutliers.long <- nooutliers %>%
+  gather(key = "group", value = "value_of_interest", group1, group2)
+head(nooutliers.long, 3)
+
+
+
+## one sided grouped t-test
+t_test_nooutliers <- nooutliers.long  %>% 
+  t_test(value_of_interest ~ group, paired = TRUE, alternative = "greater") %>% # enter "greater" or "less"
+  add_significance()
+t_test_nooutliers
 
 
 ## Paired Samples Wilcoxon Test
-stat.testFB_nooutliers <- wilcox.test(FacesVsBaseline ~ group, data = beta_FB_nooutliers.long, paired = TRUE, alternative = "less")
-stat.testFB_nooutliers # p-value = 1.822e-06
-pstat.testFB_nooutliers <- stat.testFB_nooutliers$p.value
+wilcoxon_test_nooutliers <- wilcox.test(value_of_interest ~ group, data = nooutliers.long, paired = TRUE, alternative = "less") # enter "greater" or "less"
+wilcoxon_test_nooutliers 
+pwilcoxon_test_nooutliers <- wilcoxon_test_nooutliers$p.value
 
-
-## one sided t-test
-stat.testWB_nooutliers <- beta_WB_nooutliers.long  %>% 
-  t_test(WordsVsBaseline ~ group, paired = TRUE, alternative = "greater") %>%
-  add_significance()
-stat.testWB_nooutliers # p-value = 0.0000000569
-
-stat.testWF_nooutliers <- beta_WF_nooutliers.long  %>% 
-  t_test(WordsVsFaces ~ group, paired = TRUE, alternative = "greater") %>%
-  add_significance()
-stat.testWF_nooutliers # p-value = 3.89e-25
-
-stat.testFW_nooutliers <- beta_FW_nooutliers.long  %>% 
-  t_test(FacesVsWords ~ group, paired = TRUE, alternative = "less") %>%
-  add_significance()
-stat.testFW_nooutliers # p-value = 1.75e-21
-
-
-
-# Combine summaries into a single data frame row-wise
-combined_summaries_nooutliers <- rbind(
-  data.frame(ANOVA = "WordsVsBaseline", stat.testWB_nooutliers, row.names = NULL),
-  data.frame(ANOVA = "WordsVsFaces", stat.testWF_nooutliers, row.names = NULL),
-  data.frame(ANOVA = "FacesVsBaseline", pstat.testFB_nooutliers, row.names = NULL),
-  data.frame(ANOVA = "FacesVsWords", stat.testFW_nooutliers, row.names = NULL)
-)
 
 
 # effect size (calculated with Cohen's)
-effectWB_nooutliers <- beta_WB_nooutliers.long  %>% cohens_d(WordsVsBaseline ~ group, paired = TRUE)
-effectWF_nooutliers <- beta_WF_nooutliers.long  %>% cohens_d(WordsVsFaces ~ group, paired = TRUE)
-effectFB_nooutliers <- beta_FB_nooutliers.long  %>% cohens_d(FacesVsBaseline ~ group, paired = TRUE)
-effectFW_nooutliers <- beta_FW_nooutliers.long  %>% cohens_d(FacesVsWords ~ group, paired = TRUE)
-
-
-# Combine summaries into a single data frame row-wise
-combined_effectsize_nooutliers <- rbind(
-  data.frame(ANOVA = "WordsVsBaseline", effectWB_nooutliers, row.names = NULL),
-  data.frame(ANOVA = "WordsVsFaces", effectWF_nooutliers, row.names = NULL),
-  data.frame(ANOVA = "FacesVsBaseline", effectFB_nooutliers, row.names = NULL),
-  data.frame(ANOVA = "FacesVsWords", effectFW_nooutliers, row.names = NULL)
-)
+effect_nooutliers <- nooutliers.long  %>% cohens_d(value_of_interest ~ group, paired = TRUE)
 
 
 
 ## Visualization (rename columns and create boxplot)
-# WordsVsBaseline
 
-beta_WB_nooutliers.long[beta_WB_nooutliers.long == "WordsVsBaseline_LH"] <- "LH"
-beta_WB_nooutliers.long[beta_WB_nooutliers.long == "WordsVsBaseline_RH"] <- "RH"
-
-ggboxplot(beta_WB_nooutliers.long, x = "group", y = "WordsVsBaseline",
-                ylab = "beta values", xlab = "Words>Baseline", add = "jitter")
-ggsave("boxplot_WordsVsBaseline_betaValues.png")
-
-
-# WordsVsFaces
-
-beta_WF_nooutliers.long[beta_WF_nooutliers.long == "WordsVsFaces_LH"] <- "LH"
-beta_WF_nooutliers.long[beta_WF_nooutliers.long == "WordsVsFaces_RH"] <- "RH"
-
-ggboxplot(beta_WF_nooutliers.long, x = "group", y = "WordsVsFaces",
-          ylab = "beta values", xlab = "Words>Faces", add = "jitter")
-ggsave("boxplot_WordsVsFaces_betaValues.png")
-
-
-# FacesVsBaseline
-
-beta_FB_nooutliers.long[beta_FB_nooutliers.long == "FacesVsBaseline_LH"] <- "LH"
-beta_FB_nooutliers.long[beta_FB_nooutliers.long == "FacesVsBaseline_RH"] <- "RH"
-
-ggboxplot(beta_FB_nooutliers.long, x = "group", y = "FacesVsBaseline",
-          ylab = "beta values", xlab = "Faces>Baseline", add = "jitter")
-ggsave("boxplot_FacesVsBaseline_betaValues.png")
-
-
-# FacesVsWords
-
-beta_FW_nooutliers.long[beta_FW_nooutliers.long == "FacesVsWords_LH"] <- "LH"
-beta_FW_nooutliers.long[beta_FW_nooutliers.long == "FacesVsWords_RH"] <- "RH"
-
-ggboxplot(beta_FW_nooutliers.long, x = "group", y = "FacesVsWords",
-          ylab = "beta values", xlab = "Faces>Words", add = "jitter")
-ggsave("boxplot_FacesVsWords_betaValues.png")
+ggboxplot(nooutliers.long, x = "group", y = "value_of_interest",
+                ylab = "value_of_interest, xlab = "Groups", add = "jitter")
+ggsave("boxplot_groups.png")
 
 
 
